04b536985c3efa37627593c5e370a2caa0b0dcb1
Alby-On <66146586+Alby-On@users.noreply.github.com>
2026-03-07 02:36:46 -0300

    Update productos-db.js

--- js/productos-db.js
+++ js/productos-db.js
@@ -1,9 +1,3 @@
-// 1. Configuración de Supabase
-const supabaseUrl = 'https://afrfaeouzkjdkkqeozgq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcmZhZW91emtqZGtrcWVvemdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTg1OTUsImV4cCI6MjA4Nzc5NDU5NX0.CRUaz7sNOuotsV3tVM5O2KvTerAT6uTXHaTy4yKKAdM';
-
-const _supabase = supabase.createClient(supabaseUrl, supabaseKey); 
-
 // Variable global para almacenar los productos y usarlos en los filtros
 let todosLosProductos = []; 
 
